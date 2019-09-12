e5edf84a25274f313843b44f9c0aa6a65dad680c
Baptiste Langlade <baptouuuu@gmail.com>
2019-09-12 11:50:07 +0200

    add missing thrown exceptions

--- src/Bucket.php
+++ src/Bucket.php
@@ -8,7 +8,14 @@ use Innmind\Stream\Readable;
 
 interface Bucket
 {
+    /**
+     * @throws Exception\UnableToAccessPath
+     */
     public function get(PathInterface $path): Readable;
+
+    /**
+     * @throws Exception\FailedToUploadContent
+     */
     public function upload(PathInterface $path, Readable $content): void;
     public function delete(PathInterface $path): void;
     public function has(PathInterface $path): bool;
