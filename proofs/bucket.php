d16861dd7bf31d7a0a966e98773bf96e2365032c
Baptiste Langlade <baptouuuu@gmail.com>
2024-03-16 10:32:09 +0100

    add bucket configuration for proofs

---
+++ proofs/bucket.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+use Innmind\S3\{
+    Bucket,
+    Factory,
+    Region,
+};
+use Innmind\OperatingSystem\Factory as OSFactory;
+use Innmind\Url\Url;
+use Innmind\BlackBox\Tag;
+use Symfony\Component\Dotenv\Dotenv;
+
+return static function() {
+    $file = __DIR__.'/../.env';
+
+    if (\file_exists($file)) {
+        $dotenv = new Dotenv;
+        $dotenv->load($file);
+    }
+
+    $os = OSFactory::build();
+    $bucket = Factory::of($os)->build(
+        Url::of($_ENV['S3_URL'] ?? throw new Exception('Env var missing')),
+        Region::of($_ENV['S3_REGION'] ?? throw new Exception('Env var missing')),
+    );
+
+    yield test(
+        'Bucket over HTTP interface',
+        static fn($assert) => $assert
+            ->object($bucket)
+            ->instance(Bucket::class),
+    )->tag(Tag::local, Tag::ci);
+};
